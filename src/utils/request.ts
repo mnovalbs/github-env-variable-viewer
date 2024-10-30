192ebd79a7a1f80d21a6634c886e975a7c9878ec
Noval Bintang <muhammad.salim@bfi.co.id>
2024-10-30 10:12:19 +0700

    get all env var page

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -48,24 +48,49 @@ export const getRepository = async (props: {
   return repository;
 };
 
+const getVariablesPerPage = async (props: {
+  repositoryId: number;
+  envName: string;
+  page: number
+}) => octokit.request(
+  "GET /repositories/{repository_id}/environments/{environment_name}/variables",
+  {
+    repository_id: props.repositoryId,
+    environment_name: props.envName,
+    page: props.page,
+    per_page: 30,
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  }
+);
+
 export const getVariables = async (props: {
   repositoryId: number;
   envName: string;
 }) => {
   try {
-    const {
-      data: { variables = [] },
-    } = await octokit.request(
-      "GET /repositories/{repository_id}/environments/{environment_name}/variables",
-      {
-        repository_id: props.repositoryId,
-        environment_name: props.envName,
-        per_page: 30,
-        headers: {
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      }
-    );
+    const LIMIT = 30
+    let page = 0
+    let totalCount = 30 
+    let totalPage = Math.ceil(totalCount / LIMIT)
+    let variables: {
+      name: string;
+      value: string;
+      created_at: string;
+      updated_at: string;
+    }[] = []
+
+    while (page < totalPage) {
+      page++
+      const {
+        data: { variables: variablesPerPage, total_count },
+      } = await getVariablesPerPage({repositoryId: props.repositoryId, envName: props.envName, page})
+      variables = variables.concat(variablesPerPage)
+      totalCount = total_count
+      totalPage = Math.ceil(totalCount / LIMIT)
+    }
+
     return variables;
   } catch (e) {
     return [];
